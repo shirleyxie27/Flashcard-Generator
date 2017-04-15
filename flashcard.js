d16f66252de4884a7b7a6a1ca35ffcbdbda02da6
Shirley Xie <shirleyxiesmacpro@Shirleys-MacBook-Pro.local>
2017-04-15 13:00:41 -0700

    constructors

--- flashcard.js
+++ flashcard.js
@@ -4,3 +4,16 @@ var fs = require("fs");
 var dataTxt = ("./data.json");
 var storeBasicCard = "";
 var storeClozeCard = "";
+
+//constructor for BasicCard & ClozeCard
+function BasicCard(front, back){
+    this.front = front;
+    this.back = back;
+}//end of BasicCard constructor
+
+function ClozeCard(text, cloze, partial){
+    this.text = text;
+    this.cloze = cloze;
+    this.partial = partial;
+}//end of ClozeCard constructor
+
